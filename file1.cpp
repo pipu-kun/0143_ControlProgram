c6acd545b2ef194f8c99933e362ba8f6b9c2a683
pipu-kun <SideActivity@proton.me>
2026-03-10 11:39:59 +0000

    adding file1.cpp

---
+++ file1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x == 5){
+        x = x + 1;
+        cout << "Nilai x sudah dikoreksi menjadi " << x << endl;
+    }
+
+    cout << "Bilangan yang diberikan adalah " << x << endl;
+}
